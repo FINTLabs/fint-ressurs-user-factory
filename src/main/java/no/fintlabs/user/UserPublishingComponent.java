fe570ec862083fc7b09cb28e656d45301e234e72
Eivind Morch <eivind@morch.priv.no>
2022-12-06 14:19:01 +0100

    wip

--- src/main/java/no/fintlabs/user/UserPublishingComponent.java
+++ src/main/java/no/fintlabs/user/UserPublishingComponent.java
@@ -1,12 +1,14 @@
 package no.fintlabs.user;
 
 import lombok.extern.slf4j.Slf4j;
+import no.fint.model.felles.kompleksedatatyper.Kontaktinformasjon;
 import no.fint.model.felles.kompleksedatatyper.Periode;
 import no.fint.model.resource.administrasjon.organisasjon.OrganisasjonselementResource;
 import no.fint.model.resource.administrasjon.personal.ArbeidsforholdResource;
 import no.fint.model.resource.administrasjon.personal.PersonalressursResource;
 import no.fint.model.resource.felles.PersonResource;
 import no.fintlabs.cache.FintCache;
+import no.fintlabs.links.ResourceLinkUtil;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +24,8 @@ public class UserPublishingComponent {
     private final FintCache<String, PersonalressursResource> personalressursResourceCache;
     private final FintCache<String, OrganisasjonselementResource> organisasjonselementResourceCache;
     private final FintCache<String, ArbeidsforholdResource> arbeidsforholdResourceCache;
+
+    private final UserEntityProducerService userEntityProducerService;
 
     public UserPublishingComponent(
             FintCache<String, PersonResource> personResourceCache,
@@ -39,26 +43,108 @@ public class UserPublishingComponent {
     public void doSomething() {
         Date currentTime = Date.from(Instant.now());
         personalressursResourceCache.getAllDistinct()
-                .stream();
+                .stream()
+                .filter(personalressursResource -> isValid(personalressursResource.getAnsettelsesperiode(), currentTime))
+                .filter(personalressursResource -> !personalressursResource.getArbeidsforhold().isEmpty())
+                .map(personalressursResource -> createUser(personalressursResource, currentTime));
+        // TODO: 06/12/2022 For each:
+        //  check if already published on event topic (compare hash)
+        //  publish if not already published
     }
 
-    private Optional<OrganisasjonselementResource> getOrganisasjonsElementResource(String href, Date currentTime) {
+    private Optional<User> createUser(PersonalressursResource personalressursResource, Date currentTime) {
+        Optional<PersonResource> personResourceOptional = getPersonResource(personalressursResource);
+        if (personResourceOptional.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Optional<ArbeidsforholdResource> arbeidsforholdResourceOptional = getArbeidsforholdResource(personalressursResource, currentTime);
+        if (arbeidsforholdResourceOptional.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Optional<OrganisasjonselementResource> organisasjonselementResource = arbeidsforholdResourceOptional
+                .flatMap(arbeidsforholdResource -> getOrganisasjonsElementResource(arbeidsforholdResource, currentTime));
+        if (organisasjonselementResource.isEmpty()) {
+            return Optional.empty();
+        }
+
+        // TODO: 06/12/2022 leder
+        ResourceLinkUtil.getFirstLink(
+                organisasjonselementResource.get()::getLeder,
+                organisasjonselementResource.get(),
+                "leder"
+        );
+
+        return Optional.of(createUser(
+                personalressursResource,
+                personResourceOptional.get(),
+
+        ));
+    }
+
+    private User createUser(
+            PersonalressursResource personalressursResource,
+            PersonResource personResource,
+            PersonalressursResource lederPersonalressursResource
+    ) {
+        String mobilePhone = Optional.ofNullable(personResource.getKontaktinformasjon())
+                .map(Kontaktinformasjon::getMobiltelefonnummer)
+                .orElse("");
+
+        return User
+                .builder()
+                .resourceId(ResourceLinkUtil.getFirstSelfLink(personalressursResource))
+                .firstName(personResource.getNavn().getFornavn())
+                .lastName(personResource.getNavn().getEtternavn())
+                .userType(String.valueOf(UserUtils.userType.EMPLOYEE))
+                .mobilePhone(mobilePhone)
+                .managerRef(ResourceLinkUtil.getFirstSelfLink(lederPersonalressursResource))
+                .build();
+    }
+
+    private Optional<PersonResource> getPersonResource(PersonalressursResource personalressursResource) {
+        return personResourceCache.getOptional(
+                ResourceLinkUtil.getFirstLink(
+                        personalressursResource::getPerson,
+                        personalressursResource,
+                        "Person"
+                ));
+    }
+
+    private Optional<OrganisasjonselementResource> getOrganisasjonsElementResource(ArbeidsforholdResource arbeidsforholdResource, Date currentTime) {
         return organisasjonselementResourceCache
-                .getOptional(href)
+                .getOptional(
+                        ResourceLinkUtil.getFirstLink(
+                                arbeidsforholdResource::getArbeidssted,
+                                arbeidsforholdResource,
+                                "Arbeidssted"
+                        )
+                )
                 .filter(organisasjonselementResource ->
                         isValid(organisasjonselementResource.getGyldighetsperiode(), currentTime));
     }
 
-    private Optional<ArbeidsforholdResource> getArbeidsforholdResource(String href, Date currentTime) {
+    private Optional<PersonalressursResource> getLeder(OrganisasjonselementResource organisasjonselementResource) {
+
+    }
+
+    private Optional<ArbeidsforholdResource> getArbeidsforholdResource(PersonalressursResource personalressursResource, Date currentTime) {
         return arbeidsforholdResourceCache
-                .getOptional(href)
+                .getOptional(
+                        ResourceLinkUtil.getFirstLink(
+                                personalressursResource::getArbeidsforhold,
+                                personalressursResource,
+                                "Arbeidsforhold"
+                        )
+                )
                 .filter(arbeidsforholdResource ->
-                                isValid(
-                                        arbeidsforholdResource.getArbeidsforholdsperiode() != null
-                                                ? arbeidsforholdResource.getArbeidsforholdsperiode()
-                                                : arbeidsforholdResource.getGyldighetsperiode()
-                                        currentTime
-                                )
+                        isValid(
+                                arbeidsforholdResource.getArbeidsforholdsperiode() != null
+                                        ? arbeidsforholdResource.getArbeidsforholdsperiode()
+                                        : arbeidsforholdResource.getGyldighetsperiode(),
+                                currentTime
+                        )
                 );
     }
 
