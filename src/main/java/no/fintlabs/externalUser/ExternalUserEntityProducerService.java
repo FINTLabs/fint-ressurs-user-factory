aa15a322be8bae5a1c570c2acde46ac73eb1adf6
Erling Jahr <erling.jahr@vigoiks.no>
2023-10-04 12:03:10 +0200

    added service to publish externalUsers

---
+++ src/main/java/no/fintlabs/externalUser/ExternalUserEntityProducerService.java
@@ -0,0 +1,59 @@
+package no.fintlabs.externalUser;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.cache.FintCache;
+import no.fintlabs.kafka.entity.EntityProducer;
+import no.fintlabs.kafka.entity.EntityProducerFactory;
+import no.fintlabs.kafka.entity.EntityProducerRecord;
+import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import no.fintlabs.kafka.entity.topic.EntityTopicService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class ExternalUserEntityProducerService {
+    private final FintCache<String,Integer> publishedExternalUserHashCache;
+    private final EntityProducer<ExternalUser> entityProducer;
+    private final EntityTopicNameParameters entityTopicNameParameters;
+
+    public ExternalUserEntityProducerService(
+            FintCache<String, Integer> publishedExternalUserHashCache,
+            EntityTopicService entityTopicService,
+            EntityProducerFactory entityProducerFactory
+    ){
+        this.publishedExternalUserHashCache = publishedExternalUserHashCache;
+        entityProducer = entityProducerFactory.createProducer(ExternalUser.class);
+        this.entityTopicNameParameters = EntityTopicNameParameters
+                .builder()
+                .resource("externaluser")
+                .build();
+        entityTopicService.ensureTopic(entityTopicNameParameters,0);
+    }
+
+    public List<ExternalUser> publishChangedExternalUsers(List<ExternalUser> externalUsers){
+        return externalUsers
+                .stream()
+                .filter(externalUser -> publishedExternalUserHashCache
+                        .getOptional(String.valueOf(externalUser.getIdentityProviderUserObjectId()))
+                        .map(publishedExternalUserHashCache -> publishedExternalUserHashCache != externalUser.hashCode())
+                        .orElse(true)
+                )
+                .peek(this::publishChangedExternalUser)
+                .toList();
+    }
+
+    private void publishChangedExternalUser(ExternalUser externalUser) {
+        String key = String.valueOf(externalUser.getIdentityProviderUserObjectId());
+        entityProducer.send(
+                EntityProducerRecord.<ExternalUser>builder()
+                        .topicNameParameters(entityTopicNameParameters)
+                        .key(key)
+                        .value(externalUser)
+                        .build()
+        );
+
+    }
+
+}
